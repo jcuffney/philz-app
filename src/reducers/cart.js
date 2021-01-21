e78ce752b7739c2e605887e4ceed1017e5c5bbf4
Joe Cuffney <joe.cuffney@nike.com>
2021-01-21 17:10:53 -0500

    cart quantities

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,4 +1,18 @@
-import { append, filter, equals, not, pipe, assoc } from 'ramda';
+import {
+    applyTo,
+    append,
+    filter,
+    equals,
+    not,
+    pipe,
+    assoc,
+    findIndex,
+    prop,
+    inc,
+    update,
+    dec,
+    remove
+} from 'ramda';
 
 export default (state = [], action) => {
     switch (action.type) {
@@ -8,6 +22,39 @@ export default (state = [], action) => {
             return filter(pipe(equals(action.payload), not), state)
         case 'CLEAR_CART':
             return [];
+        case 'UPDATE_QUANTITY':
+            const { item, type } = action.payload;
+            // get cart item by index (so we can update/remove it easily once the quantity is updated)
+            const itemIdx = findIndex(equals(item))(state);
+
+            // if the item isn't found; return the current state;  would likely log here as that would indicate another issue.
+            if (itemIdx === -1) return state;
+
+            // current cart item
+            const cartItem = prop(itemIdx, state);
+
+            // increment case
+            if (type == '++') { 
+                const newQuantity = applyTo(cartItem, pipe(
+                    prop('quantity'),
+                    inc,
+                ));
+                const updatedCartItem = assoc('quantity', newQuantity, cartItem);
+                return update(itemIdx, updatedCartItem, state);
+            // decrement case
+            } else if (type == '--') { 
+                const newQuantity = applyTo(cartItem, pipe(prop('quantity'), dec));
+                // if `quantity` is decremented to 0; remove it from the cart.
+                if (newQuantity === 0) remove(itemIdx, inc(itemIdx), state);
+
+                // otherwise just decrement the quantity
+                const updatedCartItem = assoc('quantity', newQuantity, cartItem);
+                return update(itemIdx, updatedCartItem, state);
+            }
+            
+            // this should never actually get hit...but is a catch all.
+            // if this were real i'd likely log something here to note the issue.
+            return state;
         default:
             return state;
     }
